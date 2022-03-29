b0915783103ce40474202daa8b84e063c2dd9623
Nirmal-Mudaliar <nirmalmudaliar22@gmail.com>
2022-03-29 23:49:16 +0530

    Added basic functions

--- app/src/main/java/com/example/noteme/feature_note/domain/model/Note.kt
+++ app/src/main/java/com/example/noteme/feature_note/domain/model/Note.kt
@@ -3,6 +3,7 @@ package com.example.noteme.feature_note.domain.model
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import com.example.noteme.ui.theme.*
+import java.lang.Exception
 
 @Entity(tableName = "note")
 data class Note(
@@ -16,3 +17,5 @@ data class Note(
         val noteColors = listOf(RedOrange, LightGreen, Violet, BabyBlue, RedPink)
     }
 }
+
+class InvalidNoteException(message: String): Exception(message)
